72cece48a1ce0596979c6c035956073cfc075d68
Vladislav <413022@niuitmo.ru>
2025-02-23 00:35:44 +0300

    added report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from input_utils import get_input_data
 from solver import simple_iteration_method, check_diagonal_dominance
 
+
 def main():
     matrix, vector = get_input_data()
 
@@ -14,6 +15,7 @@ def main():
         f"Норма матрицы: {matrix_norm}\n"
         f"Вектор погрешностей: {error_vector}"
     )
+
 
 if __name__ == "__main__":
     main()
